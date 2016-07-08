6e45a3c77215302f369754f55753948ceb22b339
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2016-07-08 08:54:24 -0400

    Cleaned up mess

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # Add repos
 add-apt-repository ppa:ubuntu-toolchain-r/test
-add-apt-repository ppa:webupd8team/java -y
+add-apt-repository ppa:george-edison55/cmake-3.x
 apt-get update
 
 # Essentials
@@ -12,18 +12,11 @@ apt-get install -y git
 # Install newest gcc and g++
 apt-get install -y gcc-6 g++-6
 
-# Install and setup Java 8
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install oracle-java8-installer
-echo "Setting environment variables for JAVA8"
-apt-get install -y oracle-java8-set-default
+# Install CMake
+apt-get install -y cmake
 
-# install unzip for Gradle installation
-apt-get install -y unzip
-
-# Extract Gradle
-wget "https://services.gradle.org/distributions/gradle-2.14-all.zip"
-unzip gradle-2.14-all.zip
+# Upgrade all
+apt-get -y upgrade
 
 # Configure git
 git config --global user.name "Omar Abdeldayem"
