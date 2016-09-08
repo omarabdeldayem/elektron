913b88e6503b0ef5228f5838dc7b5684d7523aa8
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2016-09-08 10:01:45 -0400

    Removing .cpp file from cv, updated integral image function

--- src/cv/IntegralImage.hpp
+++ src/cv/IntegralImage.hpp
@@ -3,12 +3,33 @@
 
 #include "../blas/Matrix.hpp"
 
+#include <cstddef>
+
 namespace nlib 
 {
+namespace cv
+{
 
-template <typename T>
-void compute_iimg(const Matrix<T>& mat, const Matrix<T>& iim_mat);
+// Computes the integral image (summed-area table) of matrix m
+template <typename T, std::size_t ROWS, std::size_t COLS>
+void compute_iimg(const Matrix<T, ROWS, COLS>& m, Matrix<T, ROWS, COLS>& iimg_m)
+{
+	// Required initial values	
+	iimg_m(0, 0) = m(0, 0);
+	iimg_m(0, 0) = m(0, 0) + m(0, 1);
+	iimg_m(0, 0) = m(0, 0) + m(1, 0);
 
+	for (int i = 1; i < m.rdim(); i++)
+	{
+		for (int j = 1; j < m.cdim(); j++)
+		{
+			iimg_m(i, j) = m(i, j) + iimg_m(i-1, j) + iimg_m(i, j-1) -
+				iimg_m(i-1, j-1);
+		}
+	}
 }
+
+} // end of namespace cv
+} // end of namespace nlib
 
 #endif
